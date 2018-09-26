2a7015d11c8327f5c43ae92a0e21a749ec4f0b53
Miskevych <IMiskevych@kiev.luxoft.com>
2018-09-26 17:02:08 +0300

    Fix poms for packaging

--- weatherland-web/src/main/java/com/lipatov/weatherland/web/WeatherLandApplication.java
+++ weatherland-web/src/main/java/com/lipatov/weatherland/web/WeatherLandApplication.java
@@ -1,9 +1,9 @@
-package com.lipatov.weatherland;
+package com.lipatov.weatherland.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lipatov.weatherland")
 public class WeatherLandApplication {
 
 	public static void main(String[] args) {
