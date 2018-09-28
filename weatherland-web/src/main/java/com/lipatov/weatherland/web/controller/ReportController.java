9b789c8c4437cb335f3a2c053cd738e885190a7e
maksim.lipatov <Maksim.Lipatov@ihsmarkit.com>
2018-09-28 17:56:10 +0300

    hello word

--- weatherland-web/src/main/java/com/lipatov/weatherland/web/controller/ReportController.java
+++ weatherland-web/src/main/java/com/lipatov/weatherland/web/controller/ReportController.java
@@ -2,28 +2,50 @@ package com.lipatov.weatherland.web.controller;
 
 import com.lipatov.weatherland.entity.Report;
 import com.lipatov.weatherland.service.IReportService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
-@RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-@Slf4j
+//@RestController
+// @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//@Slf4j
+@Controller
 public class ReportController {
+
+
+    // @Value("${index.message:test}")
+    private String message = "Hello World";
 
     @Autowired
     private IReportService reportService;
 
     @RequestMapping(value = "/report")
     public List<Report> getAll() {
-        log.info("Sending request to get all reports");
+        // log.info("Sending request to get all reports");
         List<Report> reports = reportService.getAll();
         reports.add(Report.builder().id("1").name("name_1").build());
         reports.add(Report.builder().id("2").name("name_2").build());
         return reports;
+    }
+
+    @RequestMapping(value = "/report/{id}")
+    public String getReportById(@PathVariable String id, Map<String, Object> model) {
+       /* log.info("Sending request to get all reports, movieId from request: {}", id);
+        List<Report> reports = reportService.getAll();
+        reports.add(Report.builder().id("1").name("name_1").build());
+        reports.add(Report.builder().id("2").name("name_2").build());*/
+
+        model.put("message", this.message);
+        model.put("index", id);
+        return   "index";
     }
 }
